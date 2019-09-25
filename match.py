f234e9ca8cd0d58742a9d64b99512985c202d7bb
endormi <ernosalo14@gmail.com>
2019-09-25 20:14:07 +0300

    Update match.py

--- match.py
+++ match.py
@@ -6,21 +6,21 @@ Match the face
 
 import face_recognition
 
-# Elon Musk image
-elon = face_recognition.load_image_file('./img/known/elon_musk.jpg')
-elon_encode = face_recognition.face_encodings(elon)[0]
+# Known image
+img = face_recognition.load_image_file('./img/known/image')
+img_encode = face_recognition.face_encodings(elon)[0]
 
 # Unknown image
 unknown = face_recognition.load_image_file(
-    './img/unknown/cooper.jpg')
+    './img/unknown/image')
 unknown_encode = face_recognition.face_encodings(unknown)[0]
 
 # Comparing faces
-results = face_recognition.compare_faces(
-    [elon_encode], unknown_encode)
+res = face_recognition.compare_faces(
+    [img_encode], unknown_encode)
 
-if results[0]:
-    print('This is Elon Musk')
+if res[0]:
+    print('This is known image')
 else:
-    print('This is NOT Elon Musk')
+    print('This is an unknown image')
     
